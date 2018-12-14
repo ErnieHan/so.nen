38ad17d63f04480123fbfb9761f2eb19da0bda4a
葉宗翰 <02090560@me.mcu.edu.tw>
2018-12-14 22:41:05 +0800

    update

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import "./App.css";
 import { HashRouter, Route } from "react-router-dom";
 import Layout from "./Components/Layout";
 import Header from "./Components/Header";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
 
 class App extends Component {
   render() {
-    const Home = <h1>HOME</h1>;
-    const About = <h1>About</h1>;
     return (
-      <HashRouter>
+      <HashRouter basename="/calendar">
         <Layout>
           <Header />
           <Route exact path="/" component={Home} />
---
+++ src/Pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import HomeSlickPhoto from "../Components/HomeSlickPhoto";
+// import News from "../Components/News";
+// import CurrentExhibition from "../Components/CurrentExhibition";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>HOMEPAGE</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
